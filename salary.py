c160512dd20ac28114ff7d88d16f9d463a879c80
Iwant2dye8976 <xuanbinh.bx204@gmail.com>
2024-09-20 00:49:38 +0700

    update hehe

--- salary.py
+++ salary.py
@@ -9,7 +9,16 @@ from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import KFold, cross_val_score
 from sklearn.neural_network import MLPRegressor
+from sklearn.ensemble import StackingRegressor
 import matplotlib.pyplot as plt
+import pickle
+
+evaluation = pd.DataFrame({'Model': [],
+                           'Root Mean Squared Error (RMSE)':[],
+                           'R-squared (training)':[],
+                           'R-squared (test)':[],
+                           '5-Fold Cross Validation':[]})
+
 df = pd.read_csv(".\\data_sets\\Salary Data.csv")
 df=df.dropna()
 df.drop_duplicates(inplace=True)
@@ -19,37 +28,48 @@ df = pd.get_dummies(df, columns=["Education Level"], drop_first=True)*1
 X = df.drop(columns=[ "Salary", "Gender"])
 y = df["Salary"]
 
+
+base_models = [
+    ('linear', LinearRegression()),
+    ('lasso', Lasso(alpha=0.1)),
+    ('mlp', MLPRegressor(hidden_layer_sizes=(50, 25), activation='relu', solver='adam', max_iter=5000))
+]
+
+meta_learner = LinearRegression()
+
+LR = LinearRegression()
+LS = Lasso(alpha=1)
+MLP = MLPRegressor(hidden_layer_sizes=(50, 25), activation='relu', solver='adam', max_iter=5000)
+STK = StackingRegressor(estimators=base_models, final_estimator=meta_learner)
+
+
+def get_Score(model, X_train, X_test, y_train, y_test, kf, model_name, eva):
+    model.fit(X_train, y_train)
+    y_pred_train = model.predict(X_train)
+    y_pred_test = model.predict(X_test)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred_test))
+    r2_train = r2_score(y_train, y_pred_train)
+    r2_test = r2_score(y_test, y_pred_test)
+    cv = cross_val_score(model,X,y,cv=kf).mean()
+    eva.loc[len(evaluation)] = [model_name, rmse, r2_train, r2_test, cv]
+    eva = evaluation.sort_values(by='5-Fold Cross Validation', ascending=False)
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-model = LinearRegression()
-model.fit(X_train, y_train)
-
 kf = KFold(n_splits=5, shuffle=True, random_state=42)
 
-y_pred = model.predict(X_test)
-mselin = mean_squared_error(y_test, y_pred)
-lin_r2 = r2_score(y_test, y_pred)
-cv = cross_val_score(model,X,y,cv=kf).mean()
-print(f'Linear Regression - MSE: {mselin}, R²: {lin_r2}, CV-Score: {cv}')
+trains = {
+    "Linear Regression": LR,
+    "Lasso": LS,
+    "Neuron Network": MLP,
+    "Stacking": STK
+}
 
-model = Lasso(alpha=1)
-model.fit(X_train, y_train)
 
-kf = KFold(n_splits=5, shuffle=True, random_state=42)
+for model_name, model in trains.items():
+    get_Score(model, X_train, X_test, y_train, y_test, kf, model_name, evaluation)
 
-y_pred = model.predict(X_test)
-mselin = mean_squared_error(y_test, y_pred)
-lin_r2 = r2_score(y_test, y_pred)
-cv = cross_val_score(model,X,y,cv=kf).mean()
-print(f'Lasso - MSE: {mselin}, R²: {lin_r2}, CV-Score: {cv}')
 
-model = MLPRegressor(hidden_layer_sizes=(50, 25), activation='relu', solver='adam', max_iter=5000)
-model.fit(X_train, y_train)
-
-y_pred = model.predict(X_test)
-mselin = mean_squared_error(y_test, y_pred)
-lin_r2 = r2_score(y_test, y_pred)
-cv = cross_val_score(model,X,y,cv=kf).mean()
-print(f'Neuron Network - MSE: {mselin}, R²: {lin_r2}, CV-Score: {cv}')
+print(evaluation)
 
 # plt.figure(figsize=(10, 6))
 # sns.scatterplot(x=y_test, y=y_pred, alpha=0.6)
@@ -60,3 +80,12 @@ print(f'Neuron Network - MSE: {mselin}, R²: {lin_r2}, CV-Score: {cv}')
 # plt.grid()
 # plt.legend()
 # plt.show()
+
+# with open('linear_regression_model.pkl', 'wb') as f:
+#     pickle.dump(linear_model, f)
+
+# with open('lasso_model.pkl', 'wb') as f:
+#     pickle.dump(lasso_model, f)
+
+# with open('neural_network_model.pkl', 'wb') as f:
+#     pickle.dump(nn_model, f)
