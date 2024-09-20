cd78db99a3d55398b039f78a122fdd1308527647
Iwant2dye8976 <xuanbinh.bx204@gmail.com>
2024-09-20 11:39:34 +0700

    Sua mot so loi nho

--- salary.py
+++ salary.py
@@ -12,6 +12,7 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.ensemble import StackingRegressor
 import matplotlib.pyplot as plt
 import pickle
+import os
 
 evaluation = pd.DataFrame({'Model': [],
                            'Root Mean Squared Error (RMSE)':[],
@@ -20,30 +21,37 @@ evaluation = pd.DataFrame({'Model': [],
                            '5-Fold Cross Validation':[]})
 
 df = pd.read_csv(".\\data_sets\\Salary Data.csv")
+
+# Xử lý dữ liệu
 df=df.dropna()
 df.drop_duplicates(inplace=True)
 df=df.drop(['Job Title'],axis=1)
-df = pd.get_dummies(df, columns=["Education Level"], drop_first=True)*1
 
-X = df.drop(columns=[ "Salary", "Gender"])
+# Ánh xạ cột 'Gender'
+df["Gender"] = df["Gender"].map({'Male': 1, 'Female': 0})
+
+# Xử lý dữ liệu cột 'Education Level'
+df = pd.get_dummies(df, columns=["Education Level"], drop_first=False)*1
+
+X = df.drop(columns=[ "Salary"])
 y = df["Salary"]
 
 
 base_models = [
     ('linear', LinearRegression()),
     ('lasso', Lasso(alpha=0.1)),
-    ('mlp', MLPRegressor(hidden_layer_sizes=(50, 25), activation='relu', solver='adam', max_iter=5000))
+    ('mlp', MLPRegressor(hidden_layer_sizes=(100, 50), activation='relu', solver='adam', max_iter=5000))
 ]
 
 meta_learner = LinearRegression()
 
 LR = LinearRegression()
-LS = Lasso(alpha=1)
-MLP = MLPRegressor(hidden_layer_sizes=(50, 25), activation='relu', solver='adam', max_iter=5000)
+LS = Lasso(alpha=10)
+MLP = MLPRegressor(hidden_layer_sizes=(100, 50), activation='relu', solver='adam', max_iter=5000)
 STK = StackingRegressor(estimators=base_models, final_estimator=meta_learner)
 
 
-def get_Score(model, X_train, X_test, y_train, y_test, kf, model_name, eva):
+def Model_Train(model, X_train, X_test, y_train, y_test, kf, model_name, eva):
     model.fit(X_train, y_train)
     y_pred_train = model.predict(X_train)
     y_pred_test = model.predict(X_test)
@@ -52,7 +60,11 @@ def get_Score(model, X_train, X_test, y_train, y_test, kf, model_name, eva):
     r2_test = r2_score(y_test, y_pred_test)
     cv = cross_val_score(model,X,y,cv=kf).mean()
     eva.loc[len(evaluation)] = [model_name, rmse, r2_train, r2_test, cv]
-    eva = evaluation.sort_values(by='5-Fold Cross Validation', ascending=False)
+    # if(abs(r2_test-cv) < .2 and r2_test >.7):
+    #     save_path = os.path.join(".\\trained models", f'{model_name}_model.pkl')
+    #     with open(save_path, 'wb') as f:
+    #       pickle.dump(model, f)
+
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 kf = KFold(n_splits=5, shuffle=True, random_state=42)
@@ -66,10 +78,10 @@ trains = {
 
 
 for model_name, model in trains.items():
-    get_Score(model, X_train, X_test, y_train, y_test, kf, model_name, evaluation)
+    Model_Train(model, X_train, X_test, y_train, y_test, kf, model_name, evaluation)
 
 
-print(evaluation)
+print(evaluation.sort_values(by='5-Fold Cross Validation', ascending=False))
 
 # plt.figure(figsize=(10, 6))
 # sns.scatterplot(x=y_test, y=y_pred, alpha=0.6)
@@ -80,12 +92,3 @@ print(evaluation)
 # plt.grid()
 # plt.legend()
 # plt.show()
-
-# with open('linear_regression_model.pkl', 'wb') as f:
-#     pickle.dump(linear_model, f)
-
-# with open('lasso_model.pkl', 'wb') as f:
-#     pickle.dump(lasso_model, f)
-
-# with open('neural_network_model.pkl', 'wb') as f:
-#     pickle.dump(nn_model, f)
